e91a98899735d0264fd0906f1f90bd8d927fc5a8
sophi <sel5881@g.rit.edu>
2025-10-01 12:53:53 -0400

    Added a new model named ReportingStructure

--- dotnet-code-challenge/CodeChallenge/obj/Debug/net6.0/CodeChallenge.AssemblyInfo.cs
+++ dotnet-code-challenge/CodeChallenge/obj/Debug/net6.0/CodeChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cdf745f68fced00e7f49374cafc6092dcc5df98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c216404d3ca2fb31c672ffb95ef078961d32f462")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
