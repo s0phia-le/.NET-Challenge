7cc14bd0c9088db5a35f19a5d7bd5dcfc2c29562
sophi <sel5881@g.rit.edu>
2025-10-01 18:22:22 -0400

    Implemented Mapper.cs

---
+++ dotnet-code-challenge/CodeChallenge.Tests/Helpers/Mapper.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using challenge.DTO;
+using challenge.Models;
+using CodeChallenge.Models;
+using CodeChallenge.Repositories;
+using CodeChallenge.Services;
+using Microsoft.Extensions.Logging;
+
+namespace CodeChallenge.Helpers
+{
+    public class Mapper : IMapper
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly ILogger<Mapper> _logger;
+
+        public Mapper(ILogger<Mapper> logger, IEmployeeRepository empRepo)
+        {
+            _employeeRepository = empRepo;
+            _logger = logger;
+        }
+
+        public Employee EmployeeDTO_To_Employee(EmployeeDto empDTO)
+        {
+            var newEmployee = new Employee()
+            {
+                FirstName = empDTO.FirstName,
+                LastName = empDTO.LastName,
+                Position = empDTO.Position,
+                Department = empDTO.Department
+            };
+
+            if (empDTO.DirectReports != null)
+            {
+                List<Employee> reports = new List<Employee>(empDTO.DirectReports.Count);
+                foreach (var empID in empDTO.DirectReports)
+                {
+                    var empRef = _employeeRepository.GetById(empID);
+                    if (empRef != null) reports.Add(empRef);
+                }
+
+                newEmployee.DirectReports = reports;
+                return newEmployee;
+            }
+            newEmployee.DirectReports = new List<Employee>();
+            return newEmployee;
+        }
+    }
+}
