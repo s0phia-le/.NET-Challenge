921bc6f08be7db9b7ac52c805679fb960fa59d70
sophi <sel5881@g.rit.edu>
2025-10-01 23:05:18 -0400

    Implemented ICompensationService interface

---
+++ dotnet-code-challenge/CodeChallenge/Services/ICompensationService.cs
@@ -0,0 +1,12 @@
+﻿using challenge.Models;
+using System;
+using challenge.DTO;
+
+namespace challenge.Services
+{
+    public interface ICompensationService
+    {
+        Compensation GetById(String id);
+        Compensation Create(CompensationDto compensationDto);
+    }
+}
