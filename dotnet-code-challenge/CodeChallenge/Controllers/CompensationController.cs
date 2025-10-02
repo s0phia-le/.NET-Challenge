6a3a471200eee208fa34fe51dbd54ca81dab7d08
sophi <sel5881@g.rit.edu>
2025-10-01 23:12:37 -0400

    Created CompensationController.cs

---
+++ dotnet-code-challenge/CodeChallenge/Controllers/CompensationController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using challenge.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using challenge.Services;
+using challenge.Models;
+
+namespace challenge.Controllers
+{
+    [Route("api/compensation")]
+    public class CompensationController : Controller
+    {
+        private readonly ILogger _logger;
+        private readonly ICompensationService _compensationService;
+
+        public CompensationController(ILogger<CompensationController> logger, ICompensationService compensationService)
+        {
+            _logger = logger;
+            _compensationService = compensationService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateCompensation([FromBody] CompensationDto compensationDto)
+        {
+            _logger.LogDebug($"Received compensation create request for '{compensationDto.EmployeeID}");
+            
+            Compensation newCompensation = _compensationService.Create(compensationDto);
+            
+            if (newCompensation == null)
+                return NotFound();
+            
+            return CreatedAtRoute("getCompensationByEmployeeId", new { employeeId = newCompensation.Employee.EmployeeId}, newCompensation);
+        }
+
+        [HttpGet("{employeeId}", Name = "getCompensationByEmployeeId")]
+        public IActionResult GetCompensationByEmployeeId(String employeeId)
+        {
+            _logger.LogDebug($"Received compensation get request for '{employeeId}'");
+
+            var compensation = _compensationService.GetById(employeeId);
+
+            if (compensation == null)
+                return NotFound();
+
+            return Ok(compensation);
+        }
+    }
+}
--- dotnet-code-challenge/CodeChallenge/obj/Debug/net6.0/CodeChallenge.AssemblyInfo.cs
+++ dotnet-code-challenge/CodeChallenge/obj/Debug/net6.0/CodeChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83eee1d2d7ffb6f0fc47a2a28d6ae5151a0b8b57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3fb5f903d2a17d5109291ff44a269c97a7f0f83")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
