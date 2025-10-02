39be7a7c7d51f31443d394c69de6506a3ba3acee
sophi <sel5881@g.rit.edu>
2025-10-01 20:45:53 -0400

    Created ReportingStructureControllerTests class

---
+++ dotnet-code-challenge/CodeChallenge.Tests/ReportingStructureControllerTests.cs
@@ -0,0 +1,20 @@
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+using CodeChallenge.Models;
+
+using CodeCodeChallenge.Tests.Integration.Extensions;
+using CodeCodeChallenge.Tests.Integration.Helpers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeCodeChallenge.Tests.Integration
+{
+    [TestClass]
+    public class ReportingStructureControllerTests
+    {
+        private static HttpClient _httpClient;
+        private static TestServer _testServer;
+    }
+}
